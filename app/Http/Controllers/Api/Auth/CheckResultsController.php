3f816ee5796e2645a929ddd032ba02427bb9cc81
pr_jli <pranjali.3898@gmail.com>
2020-02-14 14:23:18 +0530

    CheckResults-api-added

--- app/Http/Controllers/Api/Auth/CheckResultsController.php
+++ app/Http/Controllers/Api/Auth/CheckResultsController.php
@@ -15,8 +15,48 @@ class CheckResultsController extends Controller
     	//dd($request);
     	// $a=$request;
     	// $data = json_decode($a,true);
-    	dd(json_decode($request->getContent(), true));
-    	dd($data);
+    	$obj=json_decode($request->getContent(), true);
+    	//dd($obj);
+    	//print_r($obj); 
+    	// echo $obj['details']['Name'];
+    	// echo $obj['details']['RollNo'];
+    	// echo "\n";
+    	//echo $obj['result']['SessionType'];
+    	//echo count($obj['result']);
+    		$a=array();
+
+    	for($i=0; $i<sizeof($obj['result']); $i++) 
+    	{
+    		// dd(sizeof($obj['result'][$i]['COP']));
+    			if(!empty($obj['result'][$i]['COP']))
+				{
+					$c=sizeof($obj['result'][$i]['COP']);
+    					for($j=0;$j<$c;$j++)
+    					{
+    				
+    						array_push($a,$obj['result'][$i]['COP'][$j]);
+    					}
+    			}
+
+
+		}
+		if(!empty($a)){
+			$subjects= sizeof(array_unique($a));
+			//echo $subjects;
+			if($subjects>2)
+				 return response()->json([
+        "message" => "student not allowed"
+    ], 200);
+				else
+					 return response()->json([
+        "message" => "student allowed"
+    ], 200);
+		}
+		else
+			 return response()->json([
+        "message" => "student allowed"
+    ], 200);
+    	
     }
 
 }
